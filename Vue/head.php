e8bf7d6ae0d1a08aa8629a7c61dcc1c1fad30860
jpoya <justine.poyard@gmail.com>
2015-02-26 11:35:41 +0100

    découpe des pages

---
+++ Vue/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="utf-8">
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <title>Acupunture</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <!-- <script src="script.js"></script>-->
+</head>
