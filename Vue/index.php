a4da68b7f4fb1500d63c83356aba0fd10c807fb7
FabCPE <champel.fabien@gmail.com>
2015-02-26 14:20:39 +0100

    no message

--- Vue/index.php
+++ Vue/index.php
@@ -8,6 +8,11 @@
 <section class="mainContent">
     <h2>Accueil</h2>
     <p>Bla bla bla bla</p>
+    <?php
+        include ('../Modele/fctUtilisateur.php');
+        $accessBDD = new fctUtilisateur;
+        $accessBDD->connexion();
+    ?>
 </section>
 
 <?php include("footer.php"); ?>
