84b4472a01be20eb6cc077117eacfa59e69fbef7
FabCPE <champel.fabien@gmail.com>
2015-02-26 16:11:20 +0100

    access database

--- Modele/accessBDD.php
+++ Modele/accessBDD.php
@@ -1,41 +1,14 @@
 <?php
 
-	//classe d'accès à la base de données
 class accessBDD{
-	function connexionBDD(){
-		$host='127.0.0.1';
-		$user='root';
-		$password='';
-		$dbName='';
-		// Connexion à la base de données
-		$resConnexion = mysql_connect($host,$user,$password);
-		if (!$resConnexion){
-			die("<br /><b>Impossible de se connecter à la base de données:</b><br />".mysql_error());
-			return 0;
-		}
-		// Selection de la base de données de travail
-		$db_selected= mysql_select_db($dbName,$resConnexion);
-		if (!$db_selected){
-			die("<br /><b>Impossible d'utiliser la base de données: $dbName</b><br />".mysql_error($resConnexion));
-			return 0;
-		}
-		return $resConnexion;
-	}
-	
-	function deconnexionBDD(){
-		mysql_close($Connexion);
-	}
-	
-	function requeteSQL($requete){
-		$resRequeteSQL = mysql_query($requete);
-		if(!$resRequeteSQL){
-			$resultat=False;
-		}
-		else{
-			$resultat=$resRequeteSQL;
-		}
-		return $resultat;
-	}
+    public function connexionBDD(){
+        $db = null;
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=bdd_filerouge', 'fab', 'fab', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+        return $db;
+    }
 }
-
 ?>
--- Modele/fctUtilisateur.php
+++ Modele/fctUtilisateur.php
@@ -1,12 +1,17 @@
 <?php
-    require("accessBDD");
-	//fonction liées à l'utilisateur
-	class fctUtilisateur{
-        $accessBDD = new accessBDD;
-		function connexion(){
-            echo "test des ma fonction connexion";
-            $result = accessbdd->requeteSQL("ma requete sql");
-            echo $result;
+    include("accessBDD.php");
+	//fonctions liées à l'utilisateur
+
+    function connexion(){
+        $objet = new accessBDD();
+        $db = $objet->connexionBDD();
+        $resultats = $db->query('SELECT * FROM patho');
+        $resultats->setFetchMode(PDO::FETCH_OBJ);
+        while( $resultat = $resultats->fetch() )
+        {
+            echo 'meridien : '.$resultat->mer.'<br>';
         }
-	}
+        $resultats->closeCursor();
+    }
+
 ?>
